129dc7d4cbb3a21c2808428b08dac1a65f46306d
ThanhNguyenTrong <120764603+ThanhNguyenTrong@users.noreply.github.com>
2024-03-19 14:19:31 +0700

    Add files via upload

---
+++ SoChinhPhuong.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+// Hàm kiểm tra số chính phương
+int isPerfectSquare(int num) {
+	int squareRoot = sqrt(num);
+	return squareRoot * squareRoot == num;
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void countAndPrintPerfectSquares(int n) {
+	printf("Cac so chinh phuong nho hon %d la:\n", n);
+	int count = 0;
+	for (int i = 1; i < n; i++) {
+		if (isPerfectSquare(i)) {
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\nTong so chinh phuong la: %d\n", count);
+}
+
+int main() {
+	int n;
+	printf("Nhap so nguyen duong n: ");
+	scanf("%d", &n);
+
+	// Gọi hàm để đếm và in ra các số chính phương nhỏ hơn n
+	countAndPrintPerfectSquares(n);
+
+	return 0;
+}
